5950a46765c6f1d17c5ec07b25aedd2397d06b7a
rakou1986 <rakou1986@gmail.com>
2025-04-07 04:24:30 +0900

    asyncなので

--- bot4wz.py
+++ bot4wz.py
@@ -444,7 +444,7 @@ async def on_ready():
             delta = datetime.utcnow() - message.created_at.replace(tzinfo=None)
             if delta.total_seconds() < 900:
                 print("botが実行中であることをbot自身がステータスチャンネルに報告してから間もないため他のPCでbotが実行されている可能性があります。多重実行を防ぐためbotを実行せずに終了します。")
-                time.sleep(10)
+                await asyncio.sleep(10)
                 await bot.close()
                 return
 
